1c56d242427309e6e379a6bda0882516e3b1e8e7
Chen Bo-Yu <chenleo94@gmail.com>
2026-02-10 00:40:22 +0800

    Better mult-export, x-axis autofits to stimulus length

--- buttonstyler.py
+++ buttonstyler.py
@@ -3,40 +3,70 @@ from PyQt5.QtWidgets import QPushButton
 # --------------------------------------------------------
 # Highlight Button
 # --------------------------------------------------------
-def highlight_button(button: QPushButton):
-    button.setStyleSheet(
-        """
-        QPushButton {
-            background-color: #4CAF50;
-            color: white;
-            border-radius: 6px;
-            padding: 6px;
-        }
-        QPushButton::hover {
-            background-color: #45A049;
-        }
-        """
-    )
+def highlight_button(button: QPushButton, color="green"):
+    if color == "blue":
+        button.setStyleSheet(
+            """
+            QPushButton {
+                background-color: #0091EA;
+                color: white;
+                border-radius: 6px;
+                padding: 6px;
+            }
+            QPushButton::hover {
+                background-color: #0085D2;
+            }
+            """
+        )
+    else:  # Default to green
+        button.setStyleSheet(
+            """
+            QPushButton {
+                background-color: #4CAF50;
+                color: white;
+                border-radius: 6px;
+                padding: 6px;
+            }
+            QPushButton::hover {
+                background-color: #45A049;
+            }
+            """
+        )
 
 # --------------------------------------------------------
 # Enable Button
 # --------------------------------------------------------
-def enable_button(button: QPushButton, highlight=False):
+def enable_button(button: QPushButton, highlight=False, color="green"):
     button.setEnabled(True)
     if highlight == True:
-        button.setStyleSheet(
-        """
-        QPushButton {
-            background-color: #4CAF50;
-            color: white;
-            border-radius: 6px;
-            padding: 6px;
-        }
-        QPushButton::hover {
-            background-color: #45A049;
-        }
-        """
-    )
+        if color == "blue":
+            button.setStyleSheet(
+                """
+                QPushButton {
+                    background-color: #0091EA;
+                    color: white;
+                    border-radius: 6px;
+                    padding: 6px;
+                }
+                QPushButton::hover {
+                    background-color: #0085D2;
+                }
+                """
+            )
+        else:  # Default to green
+            button.setStyleSheet(
+                """
+                QPushButton {
+                    background-color: #4CAF50;
+                    color: white;
+                    border-radius: 6px;
+                    padding: 6px;
+                }
+                QPushButton::hover {
+                    background-color: #45A049;
+                }
+                """
+            )
     else:
         button.setStyleSheet("")
 
